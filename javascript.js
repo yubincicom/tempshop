6d96b0fed6e782225cfff202ff73c0f7d48b1f8c
yubincicom <60362990+yubincicom@users.noreply.github.com>
2020-02-05 09:12:53 -0800

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,8 +1,8 @@
 // toggle side navbar
 function toggleNav() {
     if (document.getElementById("sidebar").style.width == "0px") {
-        document.getElementById("sidebar").style.width = "300px";
-        document.getElementById("main").style.marginLeft = "300px";
+        document.getElementById("sidebar").style.width = "270px";
+        document.getElementById("main").style.marginLeft = "270px";
         document.getElementById("toggleIcon").src = "image/hide.png";
     }
     else {
